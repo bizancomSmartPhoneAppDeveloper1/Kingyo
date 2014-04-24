59c76bac06615c4072306eed73438af88288587b
dai-hashizume <dai-hashizume@bizan.com>
2014-04-24 15:28:23 +0900

    add test03

---
+++ kingyoTests/kingyoTests.m
@@ -0,0 +1,34 @@
+//
+//  kingyoTests.m
+//  kingyoTests
+//
+//  Created by bizan.com.mac03 on 2014/03/01.
+//  Copyright (c) 2014年 bizan.com.mac03. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface kingyoTests : XCTestCase
+
+@end
+
+@implementation kingyoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
